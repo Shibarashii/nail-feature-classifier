8f5b2f3a0f033eeaf7efa0ee0215d5ffe1298222
shiba <seanrei.ethan23@gmail.com>
2025-10-09 11:52:48 +0800

    misc: download best_models/ in download_models.py

--- src/utils/download_models.py
+++ src/utils/download_models.py
@@ -5,15 +5,20 @@ from huggingface_hub import snapshot_download
 def download_model_from_huggingface():
     huggingface_id = "shibarashii"
     huggingface_repo = "nail-disease-detection"
+
+    # Target is src/, so both best_models and output will live inside it
     target_dir = get_root_dir() / "src"
-    patterns = "output/**"
+
+    # Download both folders
+    patterns = ["best_models/**", "output/**"]
 
     snapshot_download(
         repo_id=f"{huggingface_id}/{huggingface_repo}",
         allow_patterns=patterns,
-        local_dir=target_dir)
+        local_dir=target_dir,
+    )
 
-    print(f"Downloaded finished. Located at: {target_dir}")
+    print(f"✅ Download finished. Files are in: {target_dir}")
 
 
 if __name__ == "__main__":
